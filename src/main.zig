64d33258f82777cc9134b95c792b90428fb21620
Mika Ryynänen <mikar@me.com>
2026-02-10 00:09:44 +0200

    Build improvements

--- src/main.zig
+++ src/main.zig
@@ -1,29 +1,7 @@
-const std = @import("std");
 const userdemo = @import("userdemo.zig");
-
-const allocator = std.heap.wasm_allocator;
 
 pub fn main() !void {
     return;
-}
-
-pub export fn pp_contract_version() i32 {
-    return 1;
-}
-
-pub export fn pp_alloc(len: i32) i32 {
-    if (len <= 0) return 0;
-    const size: usize = @intCast(len);
-    const buf = allocator.alloc(u8, size) catch return 0;
-    return @intCast(@intFromPtr(buf.ptr));
-}
-
-pub export fn pp_free(ptr: i32, len: i32) void {
-    if (ptr == 0 or len <= 0) return;
-    const size: usize = @intCast(len);
-    const addr: usize = @intCast(ptr);
-    const buf = @as([*]u8, @ptrFromInt(addr))[0..size];
-    allocator.free(buf);
 }
 
 pub export fn pp_init(api_version: i32, api_features: i64, screen_w: i32, screen_h: i32) i32 {
